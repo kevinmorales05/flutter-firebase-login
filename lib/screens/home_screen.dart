abebeb9be645f6286228d175819e1a03351281af
kevops <kevin.morales@jala-foundation.org>
2022-02-04 15:51:34 -0500

    Add singout button and home page with information getting

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:login_flutter/model/user_model.dart';
 import 'package:login_flutter/screens/login_screen.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -9,6 +12,22 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  User? user = FirebaseAuth.instance.currentUser;
+  UserModel loggedInUser = UserModel();
+
+  @override
+  void initState() {
+    super.initState();
+    FirebaseFirestore.instance
+        .collection("users")
+        .doc(user!.uid)
+        .get()
+        .then((value) {
+      loggedInUser = UserModel.fromMap(value.data());
+      setState(() {});
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -30,7 +49,7 @@ class _HomeScreenState extends State<HomeScreen> {
                         "assets/1.png",
                         fit: BoxFit.contain,
                       )),
-                  const Text("Welcome back",
+                  const Text("Welcome back!",
                       style: TextStyle(
                         fontSize: 20,
                         fontWeight: FontWeight.bold,
@@ -38,15 +57,15 @@ class _HomeScreenState extends State<HomeScreen> {
                   const SizedBox(
                     height: 10,
                   ),
-                  const Text(
-                    "Name",
+                  Text(
+                    "${loggedInUser.name} ${loggedInUser.lastName}",
                     style: TextStyle(
                       color: Colors.black54,
                       fontWeight: FontWeight.w500,
                     ),
                   ),
-                  const Text(
-                    "Email",
+                  Text(
+                    "Email: ${loggedInUser.email}",
                     style: TextStyle(
                       color: Colors.black54,
                       fontWeight: FontWeight.w500,
@@ -55,12 +74,19 @@ class _HomeScreenState extends State<HomeScreen> {
                   const SizedBox(
                     height: 15,
                   ),
-                  ActionChip(label: const Text("Logout"), onPressed: () {
-                    Navigator.pushReplacement(
-                context, MaterialPageRoute(builder: (context) => const LoginScreen()));
-                  })
+                  ActionChip(
+                      label: const Text("Logout"),
+                      onPressed: () {
+                        logout(context);
+                      })
                 ])),
       ),
     );
+  }
+
+  Future<void> logout(BuildContext context) async {
+    await FirebaseAuth.instance.signOut();
+    Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (context) => const LoginScreen()));
   }
 }
